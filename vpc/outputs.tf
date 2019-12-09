5896f73edcf4b0ef6e1ca0ded8218fae1d4241dc
ayaz <ayaz@gmail.com>
2019-12-08 20:14:56 -0600

    vpc terraformed

---
+++ vpc/outputs.tf
@@ -0,0 +1,11 @@
+output "public_subnets" {
+  value = "${aws_subnet.public_subnet.*.id}"
+}
+
+output "security_group" {
+  value = "${aws_security_group.test_sg.id}"
+}
+
+output "vpc_id" {
+  value = "${aws_vpc.main.id}"
+}
